f9724187ad6f8c10d6accba03f1801876055c1f6
Erjanazimov <erjan.azimov@gmail.com>
2024-10-05 19:02:52 +0600

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import config from '../config/index.json';
+
+const About = () => {
+  // const { company, about } = config;
+  // const { logo, name: companyName } = company;
+  // const { socialMedia, sections } = about;
+
+  return (
+    <div>
+      about
+    </div>
+  );
+};
+export default About;
