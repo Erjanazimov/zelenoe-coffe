14f9aadb84ae61127184df5fe867b13a5e9f7de7
Erjanazimov <erjan.azimov@gmail.com>
2024-10-05 19:11:54 +0600

    f

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,9 @@ const App = () => {
       {/*  </>*/}
       {/*</LazyShow>*/}
       <Analytics />
-        rtgktmg
+        <div style={{textAlign: 'center'}}>
+            Контакты в доработке
+        </div>
     </div>
   );
 };
